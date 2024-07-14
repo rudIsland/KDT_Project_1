f4e223d9ac45544f56c1f780a8a7d225ed2baa76
eropick <kt092805@gmail.com>
2024-07-15 08:38:40 +0900

    Add code_classification & modularization files

---
+++ Code/JS/logic.js
@@ -0,0 +1,141 @@
+import {GameObject, Ball, Paddle, Block, Vector2D} from "./object.js"
+
+$(document).ready(function(){
+    const canvas = document.getElementById("wall");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 600;
+    canvas.height = 800;
+
+    /******************************오브젝트 관리**********************************/
+
+    let ball = null; let dirVec = null; 
+    let paddle = null; let blocks = null;
+    let objects = []; //모든 오브젝트
+
+    function initObject(){
+        //공
+        let ballRadius = 20;
+        let x = canvas.width / 2;
+        let y = canvas.height / 2;
+        ball = new Ball(x,y,ballRadius,"#007bff");
+
+        //공의 방향 및 속도 조절
+        let dx = (Math.random() * 2 - 1) * 4;
+        let dy = 4; 
+        let velocity = 6.0; //속도 조절 상수
+        //길이 1의 방향벡터로 만든 뒤 속도 조절 상수 곱
+        dirVec = new Vector2D(dx,dy).normalize().mul(velocity); 
+        
+        //받침대
+        let paddleHeight = 20;
+        let paddleWidth = 100;
+        let paddleX = (canvas.width - paddleWidth) / 2;
+        let paddleY = paddleHeight + 10;
+        paddle = new Paddle(paddleX, paddleY, paddleWidth, paddleHeight,"#007bff");
+
+        //벽돌
+        blocks = new Array();
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 7; col++) {
+                let blockX = col * (75 + 5) + 30;
+                let blockY = canvas.height - (row * (30 + 5) + 30);
+                let block = new Block(blockX, blockY, 75, 30,"#007bff");
+                blocks.push(block);
+            }
+        }
+
+        //오브젝트 관리용 배열
+        objects = new Array();
+        objects.push(ball);
+        objects.push(paddle);    
+        blocks.forEach((block)=>{
+            objects.push(block);
+        });
+    }
+    initObject();
+    
+    /******************************이벤트 관리**********************************/
+
+    //키보드 이벤트
+    let rightPressed = false;
+    let leftPressed = false;
+
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
+
+    function keyDownHandler(e) {
+        if (e.key === "Right" || e.key === "ArrowRight") {
+            rightPressed = true;
+        }
+        else if (e.key === "Left" || e.key === "ArrowLeft") {
+            leftPressed = true;
+        }
+    }
+
+    function keyUpHandler(e) {
+        if (e.key === "Right" || e.key === "ArrowRight") {
+            rightPressed = false;
+        }
+        else if (e.key === "Left" || e.key === "ArrowLeft") {
+            leftPressed = false;
+        }
+    }
+
+    //충돌 감지 및 처리
+    function detectCollision(){ 
+
+        //공이 왼쪽 또는 오른쪽 벽에 부딪히면 반대로 가도록 설정
+        if (ball.point.x + dirVec.x > canvas.width - ball.radius 
+            || ball.point.x + dirVec.x < ball.radius) 
+        {
+            dirVec.x = -dirVec.x; //dx = -dx
+        }
+
+        //위쪽 벽
+        if (canvas.height - ball.radius < ball.point.y + dirVec.y) {
+            dirVec.y = -dirVec.y; //dy = -dy
+        } else if (ball.point.y+dirVec.y-ball.radius < paddle.point.y ) { //받침대와 충돌여부
+            if (ball.point.x > paddle.point.x && ball.point.x < paddle.point.x + paddle.width) {
+                dirVec.y = -dirVec.y;
+            } else { //만약 패들 밑 바닥에 부딪힐경우 이벤트
+                console.log("Game Over");
+                document.location.reload(); //initObject();
+            }
+        }
+
+        //받침대가 벽을 넘지 않도록 조정
+        let paddle_v = 7;
+        if (rightPressed && paddle.point.x < canvas.width - paddle.width) {
+            paddle.point = paddle.point.add(new Vector2D(paddle_v,0)); //paddleX += 7;
+        } else if (leftPressed && paddle.point.x > 0) {
+            paddle.point = paddle.point.add(new Vector2D(-paddle_v,0));
+        }
+
+        //block 충돌 미구현
+        
+    }
+
+    /******************************canvas 그리기**********************************/
+
+    //draw -> update 수정
+    function update(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        //그리기 위한 모든 오브젝트를 canvas 좌표계로 변환 
+        GameObject.mapCoordLB2CanvasFromList(canvas,objects);
+        objects.forEach((object)=>{ 
+            object.draw(ctx);
+        });
+        
+        //값의 변경을 위해 LB 좌표계로 변환
+        GameObject.mapCoordCanvas2LBFromList(canvas,objects);
+
+        //충돌 처리
+        detectCollision(); 
+
+        //방향 변경
+        ball.point = ball.point.add(dirVec);
+        requestAnimationFrame(update);
+    }
+    update();
+});
---
+++ Code/JS/object.js
@@ -0,0 +1,108 @@
+import { Vector2D } from "./vector.js"
+export { GameObject, Ball, Paddle, Block, Vector2D };
+
+class GameObject{
+    constructor(x,y){
+        this.point = new Vector2D(x,y);
+        this.coordinate = "LeftBottom origin";
+    }
+    draw(ctx){
+        console.log("Object"); /* 오버라이드 => 자식 클래스에 적용 */
+    }
+
+    //==> 캔버스 매퍼
+    mapCoordLB2Canvas(canvas){ 
+        let vec = this.point;
+        if(this.coordinate =="LeftBottom origin"&&typeof canvas == "object"){
+            let height = canvas.height;
+            vec.y = height - vec.y;
+            this.coordinate = "Canvas origin";
+        }
+    }//change origin LeftBottom to Canvas
+
+    mapCoordCanvas2LB(canvas){
+        let vec = this.point;
+        if(this.coordinate=="Canvas origin"&&typeof canvas == "object"){
+            let height = canvas.height;
+            vec.y = (vec.y - height)*-1;
+            this.coordinate = "LeftBottom origin";
+        }
+    }//change origin Canvas to LeftBottom
+
+    static mapCoordLB2CanvasAll(canvas,...objs){
+        objs.forEach((object)=>{
+            object.mapCoordLB2Canvas(canvas);
+        });
+    } //여러 벡터를 한 번에 좌표계 변경
+
+    static mapCoordLB2CanvasFromList(canvas,objs){
+        objs.forEach((object)=>{
+            object.mapCoordLB2Canvas(canvas);
+        });
+    } //배열로 인자 받는 경우
+
+    static mapCoordCanvas2LBAll(canvas,...objs){
+        objs.forEach((object)=>{
+            object.mapCoordCanvas2LB(canvas);
+        });
+    } //여러 벡터를 한 번에 좌표계 변경
+
+    static mapCoordCanvas2LBFromList(canvas,objs){
+        objs.forEach((object)=>{
+            object.mapCoordCanvas2LB(canvas);
+        });
+    } //배열로 인자 받는 경우
+}
+
+/* 공 */
+class Ball extends GameObject{
+    constructor(x,y,r,color){
+        super(x,y);
+        this.radius = r;
+        this.color = color
+    }
+
+    draw(ctx){
+        ctx.beginPath();
+        ctx.arc(this.point.x, this.point.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+/* 받침대 */
+class Paddle extends GameObject{
+    constructor(x,y,width,height,color){
+        super(x,y);
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw(ctx){
+        ctx.beginPath();
+        ctx.rect(this.point.x, this.point.y, this.width, this.height);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+/* 벽돌 */
+class Block extends GameObject{
+    constructor(x,y,width,height,color){
+        super(x,y);
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw(ctx){
+        ctx.beginPath();
+        ctx.rect(this.point.x, this.point.y, this.width, this.height);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
